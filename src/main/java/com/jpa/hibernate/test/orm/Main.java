84653931d11b6f600234f5aa0c12abf0d16d97f0
anil <anilk.vp@gmail.com>
2019-05-21 23:48:27 +0530

    added logging

--- src/main/java/com/jpa/hibernate/test/orm/Main.java
+++ src/main/java/com/jpa/hibernate/test/orm/Main.java
@@ -13,42 +13,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-      //  Class.forName("com.mysql.cj.jdbc.Driver");
-      //  Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hibernate_tests","ht_user","Test@123");
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-       // Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
-
-        SessionFactory factory = configuration.buildSessionFactory(ssr);
-        Session session = factory.openSession();
-
-        Transaction transaction = session.beginTransaction();
-
-        User user = null;
-        for(int i = 0; i <= 1000; i++) {
-            user = new User();
-            user.setId(i);
-            user.setUsername(DataGenerator.getString());
-            user.setPassword(DataGenerator.getString());
-            user.setFirstName(DataGenerator.getString());
-            user.setLastName(DataGenerator.getString());
-            user.setEmail(DataGenerator.getEmailId());
-            session.persist(user);
-        }
-
-
-        Query query = session.createQuery("from User ");
-        List<User> users = query.list();
-
-        for(User user1:users){
-            System.out.println(user1);
-        }
-
-        if(transaction != null){
-            transaction.commit();
-        }
-
-
+        HibernateTester tester = new HibernateTester();
+        tester.startTest();
     }
 }
