8686ab8f5fbfbd1bdf6bb4a8827f93a20c7c2afa
anil <anilk.vp@gmail.com>
2019-05-25 11:45:48 +0530

    added logging

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -12,12 +12,32 @@ FLUSH PRIVILEGES;
 
 USE hibernate_tests;
 
+
+-- // Tables for one to many mapping unidirectional  starts here // --
+
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (
-    id INTEGER NOT NULL PRIMARY KEY,
-    username VARCHAR(255) not null,
-    firstname VARCHAR(255) not null,
-    lastname VARCHAR(255) not null,
-    password VARCHAR(255) not null,
-    email VARCHAR(255)
-)
+    id INT NOT NULL PRIMARY KEY,
+    username VARCHAR(100) not null,
+    firstname VARCHAR(100) not null,
+    lastname VARCHAR(100) not null,
+    password VARCHAR(100) not null,
+    email VARCHAR(100)
+);
+
+DROP TABLE IF EXISTS address;
+CREATE TABLE adderss (
+    id INT NOT NULL PRIMARY KEY,
+    address1 VARCHAR(100),
+    address2 VARCHAR(100),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    country VARCHAR(100),
+    postalCode VARCHAR(100),
+    user_id INT NOT NULL,
+    CONSTRAINT fk_key_uid
+    FOREIGN KEY fk_user_id(user_id)
+    REFERENCES user(id)
+);
+
+-- // Tables for one to many mapping unidirectional  ends here // --
